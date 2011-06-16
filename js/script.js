c77ce47b09d9f7c3fd58154f4d9107c21ae12852
Mike Ball <mikedball@gmail.com>
2011-06-15 21:04:01 -0400

    Class-ifying EP.timepicker...ton of changes. Sloppy commit, I know.

--- js/script.js
+++ js/script.js
@@ -4,149 +4,168 @@ if (typeof EP === 'undefined' || !EP)  {
 }
 
 // timepicker
-EP.timepicker = (function($) {
+(function($) {
 
-    // helpers
-    function getPosLeft(elem) {
-        return Math.floor($(elem).position().left);
-    }
+    EP.timepicker = function(options) {
 
-    function formError(trigger, string) {
-        $(trigger).addClass('error');
-        alert(string);
-    }
+        var settings = {
 
-    function checkHours(ampm, hours) {
+                startPicker: $('#start_date_meta input.ep-timepicker'),
+                endPicker: $('#end_date_meta input.ep-timepicker'), 
+                startDropdown: $('#start_date_meta ol.ep-timepicker'),
+                endDropdown: $('#end_date_meta ol.ep-timepicker')
+            },
 
-        // if there is an am/pm specified
-        if (ampm) {
+            helpers = {
 
-            if (hours < 1 || hours > 12) { 
-                return false; 
-            }
+                getPosLeft: function(elem) {
+                    return Math.floor($(elem).position().left);
+                },
 
-        // if there is no am/pm specified
-        } else { 
-            if (hours > 23) { 
-                return false; 
-            }
-        }
-    }
+                formError: function(trigger, string) {
+                    $(trigger).addClass('error');
+                    alert(string);
+                },
 
-    function checkMinsSecs(minsOrSecs) {
-        if (minsOrSecs > 59) {
-            return false; 
-        }
-    }
+                checkHours: function(ampm, hours) {
 
-    obj = {
+                    // if there is an am/pm specified
+                    if (ampm) {
 
-        init: function () {
+                        if (hours < 1 || hours > 12) { 
+                            return false; 
+                        }
 
-            var startDatePicker = $('#start_date_meta input.ep-timepicker'),
-                endDatePicker = $('#end_date_meta input.ep-timepicker'),
-                startDateDropdown = $('#start_date_meta ol.ep-timepicker'),
-                endDateDropdown = $('#end_date_meta ol.ep-timepicker');
-                
-            this.toggleDropdown(startDatePicker, startDateDropdown);
-            this.toggleDropdown(endDatePicker, endDateDropdown);
-            this.populateInput(startDatePicker, startDateDropdown);
-            this.populateInput(endDatePicker, endDateDropdown);
-            //this.save('form#post');
-            $(startDatePicker).change(function() {
-                EP.timepicker.validateInput(startDatePicker);
-            });
-            $(endDatePicker).change(function() {
-                EP.timepicker.validateInput(endDatePicker);
-            });
-        },
-
-        toggleDropdown: function (trigger, dropdown) {
-
-            $(trigger).focus(function() {
-                $(dropdown).addClass('visible');
-                EP.timepicker.setDropdownPos(trigger, dropdown);
-            });
-        },
-
-        hideDropdown: function (dropdown) {
-            $(dropdown).removeClass('visible');
-        },
-
-        setDropdownPos: function (trigger, dropdown) {
-            $(dropdown).css('left', getPosLeft(trigger));
-        },
-
-        populateInput: function (trigger, dropdown) {
-            $(dropdown).find('li').click(function() {
-                $(this).parent().prev(trigger).attr('value', $(this).text());
-                EP.timepicker.hideDropdown(dropdown);
-            });
-        },
-
-        validateInput: function (trigger) {
-            var validTime = /^(\d{1,2}):(\d{2})(:(\d{2}))?(\s?(AM|am|PM|pm))?$/,
-                inputText = $(trigger).attr('value'),
-                matchArray = inputText.match(validTime);
-                
-            if (inputText !== '') {
-
-                if (matchArray) {
-
-                    var hours = matchArray[1],
-                        mins = matchArray[2],
-                        secs = matchArray[4],
-                        ampm = matchArray[6];
-
-                    if (checkHours(ampm, hours) === false) {
-                        formError(trigger, 'Invalid value for hours: ' + hours);
-                        return false;
+                    // if there is no am/pm specified
+                    } else { 
+                        if (hours > 23) { 
+                            return false; 
+                        }
                     }
+                },
 
-                    if (checkMinsSecs(mins) === false) {
-                        formError(trigger, 'Invalid value for minutes: ' + mins);
-                        return false;
+                checkMinsSecs: function(minsOrSecs) {
+                    if (minsOrSecs > 59) {
+                        return false; 
                     }
-
-                    if (checkMinsSecs(secs) === false) {
-                        formError(trigger, 'Invaid value for seconds: ' + secs);
-                        return false;
-                    }
-
-                } else {
-
-                    formError(trigger, 'Invalid time format: ' + inputText);
-                    return false;
                 }
-            }
 
-            $(trigger).removeClass('error');
-            return true;
-        },
+            },
 
-        save: function (formSelector) {
-            var form = $(formSelector);
-            $(formSelector).submit(function () {
-                if (EP.timepicker.validateInput() === false) {
-                    $('input#publish').removeClass('button-primary-disabled'); // Remove the 'focused' style for the Publish button
-                    $('img#ajax-loading').hide(); // re-hide the 'loading' graphic to left of Publish button
-                    alert('Please enter a valid time format.');
-                    return false;
-                } else if (EP.timepicker.validateInput() === true) {
+            pubMethods = {
+
+                init: function () {
+
+                    var tp = this;
+
+                    tp.toggleDropdown(settings.startPicker, settings.startDropdown);
+                    tp.toggleDropdown(settings.endPicker, settings.endDropdown);
+                    tp.populateInput(settings.startPicker, settings.startDropdown);
+                    tp.populateInput(settings.endPicker, settings.endDropdown);
+                    //this.save('form#post');
+                    
+                    $(settings.startPicker).change(function() {
+                        tp.validateInput(settings.startPicker);
+                    });
+                    $(settings.endPicker).change(function() {
+                        tp.validateInput(settings.endPicker);
+                    });
+                },
+
+                toggleDropdown: function (trigger, dropdown) {
+
+                    var tp = this;
+
+                    $(trigger).focus(function() {
+                        $(dropdown).addClass('visible');
+                        tp.setDropdownPos(trigger, dropdown);
+                    });
+                },
+
+                hideDropdown: function (dropdown) {
+                    $(dropdown).removeClass('visible');
+                },
+
+                setDropdownPos: function (trigger, dropdown) {
+                    $(dropdown).css('left', helpers.getPosLeft(trigger));
+                },
+
+                populateInput: function (trigger, dropdown) {
+                    $(dropdown).find('li').click(function() {
+                        $(this).parent().prev(trigger).attr('value', $(this).text());
+                        EP.timepicker.hideDropdown(dropdown);
+                    });
+                },
+
+                validateInput: function (trigger) {
+                    var validTime = /^(\d{1,2}):(\d{2})(:(\d{2}))?(\s?(AM|am|PM|pm))?$/,
+                        inputText = $(trigger).attr('value'),
+                        matchArray = inputText.match(validTime);
+                        
+                    if (inputText !== '') {
+
+                        if (matchArray) {
+
+                            var hours = matchArray[1],
+                                mins = matchArray[2],
+                                secs = matchArray[4],
+                                ampm = matchArray[6];
+
+                            if (helpers.checkHours(ampm, hours) === false) {
+                                formError(trigger, 'Invalid value for hours: ' + hours);
+                                return false;
+                            }
+
+                            if (checkMinsSecs(mins) === false) {
+                                formError(trigger, 'Invalid value for minutes: ' + mins);
+                                return false;
+                            }
+
+                            if (checkMinsSecs(secs) === false) {
+                                formError(trigger, 'Invaid value for seconds: ' + secs);
+                                return false;
+                            }
+
+                        } else {
+
+                            formError(trigger, 'Invalid time format: ' + inputText);
+                            return false;
+                        }
+                    }
+
+                    $(trigger).removeClass('error');
                     return true;
+                },
+
+                save: function (formSelector) {
+                    var form = $(formSelector);
+                    $(formSelector).submit(function () {
+                        if (EP.timepicker.validateInput() === false) {
+                            $('input#publish').removeClass('button-primary-disabled'); // Remove the 'focused' style for the Publish button
+                            $('img#ajax-loading').hide(); // re-hide the 'loading' graphic to left of Publish button
+                            alert('Please enter a valid time format.');
+                            return false;
+                        } else if (EP.timepicker.validateInput() === true) {
+                            return true;
+                        }
+                    });
                 }
-            });
+            };
+
+        if (options) {
+            $.extent(settings, options);
         }
+
+        return pubMethods;
     };
 
-    return obj;
-
-})(jQuery);
+}(jQuery));
 
 // what to call on doc ready 
 (function($) {
     $(document).ready(function() {
         $('input.ep-datepicker').datepicker();
-        EP.timepicker.init();
+        var timepicker = new EP.timepicker();
+        timepicker.init();
     });
 })(jQuery);
